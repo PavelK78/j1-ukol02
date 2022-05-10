583128a7cd8f14bcadc1bda8b2d837a8fbc3dd94
Filip Jirsák <filip@jirsak.org>
2022-05-10 19:22:38 +0200

    Úprava pojmenování proměnných.

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -4,10 +4,32 @@ import cz.czechitas.turtle.engine.*;
 
 public class HlavniProgram {
 
-    public void main(String[] args) {
-        Turtle zofka;
+    Turtle zofka;
 
-        zofka = new Turtle();
+    public void main(String[]args) {
+        zofka=new Turtle();
+
+        Color cervenaBarva;
+        cervenaBarva=new Color(255,0,0);
+
+        zofka.setLocation(100.0,100.0);
+        // Volani metody, do ktere se preda hodnota 50.0 a objekt, ktery je v promenne cervenaBarva
+        nakresliBarevnyRovnostrannyTrojuhelnik(50.0,cervenaBarva);
+
+        zofka.setLocation(300.0,100.0);
+        // Volani metody, do ktere se preda hodnota 70.0 a in-line vytvoreny objekt barvy
+        nakresliBarevnyRovnostrannyTrojuhelnik(70.0,new Color(0,127,255));
+    }
+
+    public void nakresliBarevnyRovnostrannyTrojuhelnik(double velikostStrany,Color barvaCary) {
+        // Zde lze používat proměnnou velikostStrany a barvaCary:
+        zofka.setPenColor(barvaCary);
+        zofka.move(velikostStrany);
+        zofka.turnLeft(120.0);
+        zofka.move(velikostStrany);
+        zofka.turnLeft(120.0);
+        zofka.move(velikostStrany);
+        zofka.turnLeft(120.0);
     }
 
 }
