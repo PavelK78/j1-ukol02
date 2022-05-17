3d57949d317ebbb4b0d40abeee9a5f665afe1e4b
PavelK78 <pavel.krejcir@outlook.com>
2022-05-17 20:42:22 +0200

    Druhý commit

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -2,25 +2,30 @@ package cz.czechitas.turtle;
 
 import cz.czechitas.turtle.engine.*;
 
+import java.awt.*;
+
 public class HlavniProgram {
 
     Turtle zofka;
 
     public void main(String[]args) {
-        zofka=new Turtle();
+        zofka = new Turtle();
+
+        Color cervenaBarva;
+        cervenaBarva = new Color(255,0,0);
 
         zofka.setLocation(100.0,100.0);
-        // Volani metody, do ktere se preda hodnota 50.0
-        nakresliRovnostrannyTrojuhelnik(50.0);
+        // Volani metody, do ktere se preda hodnota 50.0 a objekt, ktery je v promenne cervenaBarva
+        nakresliBarevnyRovnostrannyTrojuhelnik(50.0,cervenaBarva);
 
         zofka.setLocation(300.0,100.0);
-        // Volani metody, do ktere se preda hodnota 70.0
-        nakresliRovnostrannyTrojuhelnik(70.0);
+        // Volani metody, do ktere se preda hodnota 70.0 a in-line vytvoreny objekt barvy
+        nakresliBarevnyRovnostrannyTrojuhelnik(70.0,new Color(0,127,255));
     }
 
-    public void nakresliRovnostrannyTrojuhelnik(double velikostStrany) {
-        // Zde lze používat proměnnou velikostStrany.
-        // Jeji hodnota je takova, s jakou byla metoda zavolana
+    public void nakresliBarevnyRovnostrannyTrojuhelnik(double velikostStrany, Color barvaCary) {
+        // Zde lze používat proměnnou velikostStrany a barvaCary:
+        zofka.setPenColor(barvaCary);
         zofka.move(velikostStrany);
         zofka.turnLeft(120.0);
         zofka.move(velikostStrany);
@@ -28,6 +33,5 @@ public class HlavniProgram {
         zofka.move(velikostStrany);
         zofka.turnLeft(120.0);
     }
-
 }
 
